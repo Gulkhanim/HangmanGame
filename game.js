1c103e035ee83732d5e91df545c078c767ab45ca
Gulkhanim <guluhasanzade7@gmail.com>
2023-05-15 01:41:04 +0400

    update

--- game.js
+++ game.js
@@ -2,5 +2,55 @@ const wordsection = document.querySelector(".word")
 const boardsection = document.querySelector(".board")
 const figure = document.querySelector(".figure")
 const letters = [
-    "A","B". "C","D", "E"
-]
+    "A",
+    "B",
+    "C",
+    "Ç",
+    "D",
+    "E",
+    "Ə",
+    "F",
+    "G",
+    "Ğ",
+    "H",
+    "X",
+    "I",
+    "İ",
+    "J",
+    "K",
+    "Q",
+    "L",
+    "M",
+    "N",
+    "O",
+    "Ö",
+    "P",
+    "R",
+    "S",
+    "Ş",
+    "T",
+    "U",
+    "Ü",
+    "V",
+    "Y",
+    "Z",
+];
+
+const human = ["head", "body","rightarm","leftarm","rightleg","leftleg"];
+
+ 
+let randomword = "";
+
+const createkeyboard=()=>{
+    boardsection.innerHTML=""
+    for(let a=0;a<letters.length;a++)
+    {
+        let square=document.createElement("div")
+        square.classList.add("lettersquare")
+        square.textContent=letters[a]
+        boardsection.appendChild(square)
+
+    }
+}
+
+createkeyboard();
